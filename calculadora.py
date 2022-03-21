0491b113a34b04c6c4b67718eea8a2d709090cd9
Duando <duando@protonmail.com>
2022-03-21 01:35:44 +0000

    arreglos de código

--- calculadora.py
+++ calculadora.py
@@ -1,7 +1,9 @@
+# Creación de variables
 a = 2 + 2 # retorna un entero
 b = 50 - 5*6 # retorna un entero
 c = (50 - 5*6)/4
 d = 8/5 # la división siempre retorna un flotante
+# Impresión de resultados
 print("2 + 2 = ", a)
 print("50 - 5*6 = ", b)
 print("(50 - 5*6)/4 = ",c)
